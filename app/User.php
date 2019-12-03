5b2958a4f5b11bfb354f275b16ffbf91d9590731
Md. Riaz Hossain Fahad <mdriazhossainfahad@gmail.com>
2019-12-03 12:18:03 +0600

    JWT added for authentication with routes

--- app/User.php
+++ app/User.php
@@ -3,11 +3,12 @@
 namespace App;
 
 use App\Question;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
@@ -38,7 +39,28 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function question() {
+    public function question()
+    {
         return $this->hasMany(Question::class);
+    }
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
